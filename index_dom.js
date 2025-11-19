0e254c7ebab447cf88a9b1b223ea1991df623eee
Alexandro Soto <wolf.hunter.979@gmail.com>
2025-11-19 13:48:38 -0600

    Cuenta Regresiva (countdown)

--- index_dom.js
+++ index_dom.js
@@ -1,3 +1,4 @@
+import countdown from "./dom/cuenta_regresiva.js";
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock,alarm } from "./dom/reloj.js";
 import { shortcuts } from "./dom/teclado.js";
@@ -8,13 +9,16 @@ const d = document;
 d.addEventListener("DOMContentLoaded", (e)=>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
-    alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma")
+    alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
+    countdown("countdown","Jun 03, 2027 03:23:19","Feliz Cumpleños amigo y docente digital");
 });
 
 d.addEventListener("keydown",e => {
     shortcuts(e);
     moveBall(e, ".ball", ".stage");
 })
+
+
 
 
 
